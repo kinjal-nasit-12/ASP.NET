92850698666a34ee51828529bff7d233b4ef67b1
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2025-03-05 08:39:45 +0530

    Datalist-control

---
+++ U1P9/U1P9.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="U1P9.aspx.cs" Inherits="U1P9" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="ITEM NAME: "></asp:Label>
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+        <br />
+        <br />
+        <asp:DataList ID="DataList1" runat="server">
+            <ItemTemplate>
+                <asp:Image ID="Image1" runat="server" 
+                    ImageUrl='<%# Eval("Name:","~/Images/{0}") %>' />
+            </ItemTemplate>
+        </asp:DataList>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ U1P9/U1P9.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+public partial class U1P9 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile == false)
+        {
+            Response.Write("upload file");
+        }
+        else
+        {
+            string ext = Path.GetExtension(FileUpload1.FileName);
+            if (ext.ToLower() == ".jepg" || ext.ToLower() == ".jpg" || ext.ToLower() == ".bmp" || ext.ToLower() == ".png" || ext.ToLower() == ".gif")
+            {
+                if (FileUpload1.PostedFile.ContentLength <= 1024)
+                {
+                    FileUpload1.SaveAs(Server.MapPath("~/Images/") + FileUpload1.FileName);
+                    DirectoryInfo dir = new DirectoryInfo(Server.MapPath("~/Images/"));
+                    DataList1.DataSource = dir.GetFiles();
+                    DataList1.DataBind();
+                }
+                else
+                    Response.Write("size"); 
+            }
+            else
+                Response.Write("extension erro");
+        }
+    }
+}
